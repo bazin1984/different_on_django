b8c198dc9c844a4f5a9c6aecb7bd6aa3bb104b3b
Bazin1984 <bazin1984@mail.ru>
2022-11-01 19:58:28 +0300

    add_static

--- nicesite/nicesite/settings.py
+++ nicesite/nicesite/settings.py
@@ -117,6 +117,11 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.1/howto/static-files/
 
 STATIC_URL = '/static/'
+STATIC_ROOT = os.path.join(BASE_DIR , 'static')
+STATICFILES_DIRS = [
+    os.path.join(BASE_DIR , 'nicesite/static'),
+]
+
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
@@ -124,3 +129,5 @@ STATIC_URL = '/static/'
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 MEDIA_ROOT = os.path.join(BASE_DIR , 'media')
 MEDIA_URL = '/media/'
+
+
